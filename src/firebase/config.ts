a720bcbe9d5fa19a40dad1f7db80c4a397d83fa2
Naお UwU <sfyuu4598@gmail.com>
2025-11-03 20:46:17 +0000

    my api key was compromized fix it now

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,8 @@
+
 export const firebaseConfig = {
   "projectId": "studio-5785792637-546f1",
   "appId": "1:1041815318927:web:eeffa24a495a821738dbfa",
-  "apiKey": "AIzaSyCnM7ZKk5vLI5uXxQmb87PFkKvDzlIXtS0",
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   "authDomain": "studio-5785792637-546f1.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "1041815318927"
