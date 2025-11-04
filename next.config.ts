65eccf3da6b1f43de8a93ce56aa924723d7e4e8c
Naお UwU <sfyuu4598@gmail.com>
2025-11-04 10:54:12 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -41,6 +41,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.blinkist.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
